79850985e80a9f676c7086ce9b6fe6c7cbb86f0e
Andrew Batallones <batallonesa@gmail.com>
2023-12-27 15:02:46 -0500

    arrange funcs for more readability

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -6,6 +6,29 @@ import (
 	"github.com/andrewbatallones/monkey_interpreter/token"
 )
 
+type lexerTest struct {
+	expectedType    token.TokenType
+	expectedLiteral string
+}
+
+func runTests(input string, tests []lexerTest, t *testing.T) {
+	t.Helper()
+
+	l := New(input)
+
+	for i, tt := range tests {
+		tok := l.NextToken()
+
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - tokentype wrong. expected %q, got %q", i, tt.expectedType, tok.Type)
+		}
+
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected %q, got %q", i, tt.expectedLiteral, tok.Literal)
+		}
+	}
+}
+
 func TestNextTokenSymbols(t *testing.T) {
 	input := `=+(){},;`
 
@@ -111,27 +134,4 @@ if (5 < 10) {
 	}
 
 	runTests(input, tests, t)
-}
-
-type lexerTest struct {
-	expectedType    token.TokenType
-	expectedLiteral string
-}
-
-func runTests(input string, tests []lexerTest, t *testing.T) {
-	t.Helper()
-
-	l := New(input)
-
-	for i, tt := range tests {
-		tok := l.NextToken()
-
-		if tok.Type != tt.expectedType {
-			t.Fatalf("tests[%d] - tokentype wrong. expected %q, got %q", i, tt.expectedType, tok.Type)
-		}
-
-		if tok.Literal != tt.expectedLiteral {
-			t.Fatalf("tests[%d] - literal wrong. expected %q, got %q", i, tt.expectedLiteral, tok.Literal)
-		}
-	}
 }

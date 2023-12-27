fbb5f81c2896da2e0a52ff787377657d94a94133
Andrew Batallones <batallonesa@gmail.com>
2023-12-27 12:54:12 -0500

    add logic to handle test case

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -48,7 +48,6 @@ let add = fn(x, y) {
 };
 
 let result = add(five, ten);
-
 !/*5;
 5 < 10 > 5;
 `
@@ -92,6 +91,17 @@ let result = add(five, ten);
 		{token.COMMA, ","},
 		{token.IDENT, "ten"},
 		{token.RPAREN, ")"},
+		{token.SEMICOLON, ";"},
+		{token.BANG, "!"},
+		{token.SLASH, "/"},
+		{token.ASTERISK, "*"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.GT, ">"},
+		{token.INT, "5"},
 		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
 	}

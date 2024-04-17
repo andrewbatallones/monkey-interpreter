b23d859c32006a682b00f5a4a067354b4e588237
Andrew Batallones <batallonesa@gmail.com>
2024-04-17 10:18:47 -0400

    add ast struct for int

--- ast/ast.go
+++ ast/ast.go
@@ -53,6 +53,15 @@ func (i *Identifier) expressionNode()      {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
 func (i *Identifier) String() string       { return i.Value }
 
+type IntegerLiteral struct {
+	Token token.Token
+	Value int64
+}
+
+func (i *IntegerLiteral) expressionNode()      {}
+func (i *IntegerLiteral) TokenLiteral() string { return i.Token.Literal }
+func (i *IntegerLiteral) String() string       { return i.Token.Literal }
+
 type LetStatement struct {
 	Token token.Token // the token.LET token
 	Name  *Identifier

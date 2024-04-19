bb10effa783366f660f57c9b4dbb3199faa4b658
Andrew Batallones <batallonesa@gmail.com>
2024-04-19 16:15:47 -0400

    add prefix ast

--- ast/ast.go
+++ ast/ast.go
@@ -124,3 +124,23 @@ func (es *ExpresssionStatement) String() string {
 
 	return ""
 }
+
+type PrefixExpression struct {
+	Token    token.Token
+	Operator string
+	Right    Expression
+}
+
+func (pe *PrefixExpression) statementNode()       {}
+func (pe *PrefixExpression) TokenLiteral() string { return pe.Token.Literal }
+
+func (pe *PrefixExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("(")
+	out.WriteString(pe.Operator)
+	out.WriteString(pe.Right.String())
+	out.WriteString(")")
+
+	return out.String()
+}
